296543e0a541dd6c744ea0972b04f2e193d6bb78
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-07-10 18:44:37 +0400

    feat: change config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -10,7 +10,11 @@ import reactCompiler from 'eslint-plugin-react-compiler';
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.strict, eslintPluginPrettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,15 +28,20 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       'react-compiler/react-compiler': 'error',
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      '@typescript-eslint/explicit-function-return-type': ['error'],
+      '@typescript-eslint/explicit-module-boundary-types': ['error'],
     },
     settings: {
       react: {
         version: 'detect',
       },
     },
-  },
+  }
 );
