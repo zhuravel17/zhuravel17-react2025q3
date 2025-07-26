4b5880593d89ec53ad7be75ba1cbd30834d05479
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-07-26 20:38:13 +0400

    feat: add page to fetch

--- app/src/utils/fetchResults.ts
+++ app/src/utils/fetchResults.ts
@@ -1,13 +1,19 @@
 import { Character } from '../types/character';
 
-export default function fetchCharacters(term: string): Promise<Character[]> {
-  const query = term ? `?name=${term}` : '';
+export default function fetchCharacters(
+  term: string,
+  page = 1
+): Promise<{ results: Character[]; pages: number }> {
+  const query = term ? `?name=${term}&page=${page}` : `?page=${page}`;
   const url = `https://rickandmortyapi.com/api/character${query}`;
 
   return fetch(url).then(async (res) => {
-    if (res.status === 404) return [];
+    if (res.status === 404) return { results: [], pages: 1 };
     if (!res.ok) throw new Error(`Error ${res.status}`);
     const data = await res.json();
-    return data.results || [];
+    return {
+      results: data.results || [],
+      pages: data.info?.pages || 1,
+    };
   });
 }
