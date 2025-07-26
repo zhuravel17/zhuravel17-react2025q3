2b5b2c6e018dc1cb8becfb1047c3d9931bfd4c3c
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-07-26 20:55:56 +0400

    feat: add navigation to card

--- app/src/components/Card/Card.tsx
+++ app/src/components/Card/Card.tsx
@@ -1,14 +1,21 @@
 import { ReactElement } from 'react';
 import { Character } from '../../types/character';
 import './Card.styles.css';
+import { useNavigate, useParams } from 'react-router-dom';
 
 interface Props {
   item: Character;
 }
 
 export function Card({ item }: Props): ReactElement {
+  const navigate = useNavigate();
+  const { page } = useParams();
+
+  const handleClick = (): void => {
+    navigate(`/${page}/${item.id}`);
+  };
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       <img src={item.image} alt={item.name} className="card__img" />
       <div className="card__info">
         <h3 className="card__name">{item.name}</h3>
