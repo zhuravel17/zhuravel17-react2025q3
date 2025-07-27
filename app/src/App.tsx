08d85e55e36ad0b615352fd38cabd483c1fcf16b
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-07-27 17:41:08 +0400

    feat: add routes and configs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,19 @@
-import { Component, ReactNode } from 'react';
 import { MainPage } from './components/Main/Main';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { CharacterDetails } from './components/CharacterDetails/CharacterDetails';
+import { NotFound } from './components/NotFound/NotFound';
+import { About } from './components/About/About';
+import { ReactElement } from 'react';
 
-export class App extends Component {
-  render(): ReactNode {
-    return <MainPage />;
-  }
+export function App(): ReactElement {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/1" replace />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/:page" element={<MainPage />}>
+        <Route path=":detailsId" element={<CharacterDetails />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   );
 } else {
