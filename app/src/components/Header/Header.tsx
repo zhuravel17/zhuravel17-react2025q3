1cff2c81aa1ff91c652f702fb57c1d6d5ed0cfd8
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-07-27 16:46:09 +0400

    feat: add about component

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { ReactElement } from 'react';
 import './Header.styles.css';
+import { Link } from 'react-router-dom';
 
 export function Header(): ReactElement {
   return (
     <header className="header">
       <h1>Rick and Morty</h1>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
     </header>
   );
 }
