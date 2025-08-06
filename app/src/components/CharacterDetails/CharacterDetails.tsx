88f79eecf32886097b069d59a3e78218cddb0417
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-08-07 01:21:33 +0400

    feat: add query to characterDetails

--- app/src/components/CharacterDetails/CharacterDetails.tsx
+++ app/src/components/CharacterDetails/CharacterDetails.tsx
@@ -1,29 +1,15 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import { ReactElement, useEffect, useState } from 'react';
-import { Character } from '../../types/character';
+import { ReactElement } from 'react';
 import { Loading } from '../Loading/Loading';
 import './CharacterDetails.styles.css';
-import { API_URL } from '../../consts/urlConst';
+import { useGetCharacterByIdQuery } from '../../api/apiSlice';
 
 export function CharacterDetails(): ReactElement {
   const { detailsId } = useParams();
   const navigate = useNavigate();
-  const [character, setCharacter] = useState<Character>();
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (!detailsId) return;
-
-    fetch(`${API_URL}/${detailsId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setCharacter(data);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  }, [detailsId]);
+  const id = detailsId ? parseInt(detailsId, 10) : 0;
+  const { data: character, isLoading, isError } = useGetCharacterByIdQuery(id);
 
   const handleClose = (): void => {
     navigate(`..`, { relative: 'path' });
@@ -31,9 +17,9 @@ export function CharacterDetails(): ReactElement {
 
   return (
     <div className="details">
-      {loading && <Loading />}
-      {!character && <p>Character not found</p>}
-      {character && !loading && (
+      {isLoading && <Loading />}
+      {isError && <p>Character not found</p>}
+      {character && !isLoading && (
         <div>
           <button onClick={handleClose}>Close</button>
           <h2>{character?.name}</h2>
